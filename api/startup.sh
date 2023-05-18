5ed8abcc5754d1accf57daad1853488e3b65b3f1
matt-gn <matthew.g.noojin@googlemail.com>
2023-05-18 09:22:41 -0500

    Switched app server to gunicorn

--- api/startup.sh
+++ api/startup.sh
@@ -6,4 +6,4 @@ python make_gifs.py
 python db/init.py
 touch output.log
 crond
-uvicorn api:app --proxy-headers --host=0.0.0.0 --port=8000
+gunicorn api:app --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:8000
